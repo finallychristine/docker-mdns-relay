d54a2dd45bba79c8f5b6fc7ba39cf4fd6030f9b2
Christine Thornton <237637779+finallychristine@users.noreply.github.com>
2026-08-02 12:36:22 -0700

    Configure Homebrew service from config file

--- Formula/docker-mdns-relay.rb
+++ Formula/docker-mdns-relay.rb
@@ -1,20 +1,21 @@
 class DockerMdnsRelay < Formula
   desc "Bridge mDNS traffic between macOS and Docker Desktop networks"
-  homepage "https://github.com/christine/docker-mdns-relay"
-  url "https://github.com/christine/docker-mdns-relay.git",
-      tag:      "v0.0.1",
-      revision: "0a94751be750e99ef55244a6883b809c4e135134"
-  version "0.0.1"
+  homepage "https://github.com/finallychristine/docker-mdns-relay"
+  url "https://github.com/finallychristine/docker-mdns-relay.git",
+      tag:      "v0.0.2",
+      revision: "8e3d7c3761749fd0681a37ff7904a9a0c2898c02"
+  version "0.0.2"
 
   depends_on "python@3.13"
 
   def install
     bin.install "docker_mdns_relay.py" => "docker-mdns-relay"
     rewrite_shebang detected_python_shebang, bin/"docker-mdns-relay"
+    etc.install "docker-mdns-relay.conf"
   end
 
   service do
-    run [opt_bin/"docker-mdns-relay", "host", "--interface", "en0"]
+    run [opt_bin/"docker-mdns-relay", "host", "--config", etc/"docker-mdns-relay.conf"]
     keep_alive true
     log_path var/"log/docker-mdns-relay.log"
     error_log_path var/"log/docker-mdns-relay.log"
